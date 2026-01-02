568948dc0568172d6b47ada72bd6a0f0fd9b2e69
MuGi-131 <munkhbayar.mugi@gmail.com>
2026-01-02 23:17:33 +0900

    asteroid now splits

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             for shot in shots:
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
             if asteroid.collides_with(player):
