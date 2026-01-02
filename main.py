4fa56d8e68bae61d7e86952a888eea5ea5d8b738
MuGi-131 <munkhbayar.mugi@gmail.com>
2026-01-02 13:56:15 +0900

    rendering by group class

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -20,8 +23,9 @@ def main():
 
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
