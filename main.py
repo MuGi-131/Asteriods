fd8a794ac4c3af12dcb0f1ccbe754e0a458ca833
MuGi-131 <munkhbayar.mugi@gmail.com>
2026-01-02 22:57:07 +0900

    asteroid and shot collision

--- main.py
+++ main.py
@@ -39,6 +39,13 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
             if asteroid.collides_with(player):
                 log_event("player_hit")
                 print("Game over!")
